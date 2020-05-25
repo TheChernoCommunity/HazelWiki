4eb5baf15e2e7b0bb14cbcf28555d921e144b88a
LovelySanta <laurens.scheldeman@gmail.com>
2020-05-25 18:24:40 +0200

    small tweaks and features

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
-    <navigation-bar :toc="navBarLayout" />
-    <overlay />
-    <top-header />
-    <router-view/>
-  </div>
+	<div>
+		<navigation-bar :toc="HazelWikiLayout" />
+		<overlay />
+		<top-header />
+		<router-view/>
+	</div>
 </template>
 
 <script>
-  import NavBar from './components/NavBar/NavBar';
-  import Header from './components/Header/Header';
-  import Overlay from "./components/Overlay/Overlay";
+	import NavBar from './components/NavBar/NavBar';
+	import Header from './components/Header/Header';
+	import Overlay from "./components/Overlay/Overlay";
 
-  import '../node_modules/materialize-css/dist/css/materialize.css';
-  import '../node_modules/materialize-css/dist/js/materialize.js';
+	import '../node_modules/materialize-css/dist/css/materialize.css';
+	import '../node_modules/materialize-css/dist/js/materialize.js';
 
-  const navBarLayout = require('./assets/NavBarLayout.json');
+	const HazelWikiLayout = require('@/assets/HazelWikiLayout.json');
 
-  export default {
-    name: 'App',
-    data() {
-      return {
-        navBarLayout: navBarLayout,
-      }
-    },
-    components: {
-      'overlay': Overlay,
-      'top-header': Header,
-      'navigation-bar': NavBar,
-    }
-  }
+	export default {
+		name: 'App',
+		data() {
+			return {
+				HazelWikiLayout: HazelWikiLayout,
+			}
+		},
+		components: {
+			'overlay': Overlay,
+			'top-header': Header,
+			'navigation-bar': NavBar,
+		}
+	}
 </script>
 
 
