90a7f013639aa20dbb15165833addd626273aa89
Jackson Silva Fernandes <silvajackson50@gmail.com>
2021-02-21 05:04:36 -0400

    Improvement on the Responsivity and the UI (#10)

--- src/views/BasicPage.vue
+++ src/views/BasicPage.vue
@@ -1,11 +1,13 @@
 <template>
-	<div class="container">
+	<div class="container" v-bind:style="{ paddingLeft: paddingLeft+'px' }">
 		<markdown-interpreter :src="src" />
 	</div>
 </template>
 
 <script>
 	import MarkdownInterpreter from '@/components/MarkdownInterpreter/MarkdownInterpreter.vue';
+	import NavBar from  "@/scss/NavBar.scss";
+	import Utils from '@/utils/Utils.js';
 
 	export default {
 		props: {
@@ -14,14 +16,41 @@
 				required: true
 			}
 		},
+		data() {
+			return {
+				paddingLeft: 0,
+			}
+		},
 		components: {
 			'markdown-interpreter': MarkdownInterpreter,
+		},
+		created() {
+			if(!Utils.isLayoutCompact()) {
+				this.paddingLeft = NavBar.width;
+			}
+		},
+		mounted() {
+			this.$nextTick(() => {
+				window.addEventListener('resize', this.onResize);
+			})
+		},
+		methods: {
+			onResize() {
+				if(Utils.isLayoutCompact()) {
+					this.paddingLeft = 0;
+				} else {
+					this.paddingLeft = NavBar.width;
+				}
+			}
+		},
+		beforeDestroy() {
+			window.removeEventListener('resize', this.onResize);
 		}
 	}
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
 	.container {
-		padding-top: 5px;
+		padding-top: 32px;
 	}
 </style>
